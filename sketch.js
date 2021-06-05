7e9f4fc212d1edf384fca6e9b29c9c4f92b06b35
nishtha2008 <79771845+nishtha2008@users.noreply.github.com>
2021-06-05 12:50:23 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,8 +28,9 @@ function setup(){
     log4 = new Log(760,120,150,PI/7);
     log5 = new Log(870,120,150,-PI/7);
     bird = new Bird(100,100,);
-    log6 = new Log(230,180,80,PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80,PI/2);
+    slingshot = new SlingShot(bird.body,{x:200,y:100});
+
 }
 
 function draw(){
@@ -52,6 +53,12 @@ function draw(){
     log5.display();
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();
+    //log6.display();
+    slingshot.display();
+}
+function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+slingshot.fly();
 }
