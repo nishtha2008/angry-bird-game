55c7b857e43e78692ea47cc3f928f3081e782e80
nishtha2008 <79771845+nishtha2008@users.noreply.github.com>
2021-06-12 12:59:58 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -62,3 +62,8 @@ Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
 function mouseReleased(){
 slingshot.fly();
 }
+function keyPressed(){
+ if(keyCode===32){
+ slingshot.attach(bird.body);
+ }
+}
